2d1295741f181bfc7673f1bec444917f61ac5dc0
Prajwal <rohitjagtap5000@gmail.com>
2024-10-03 15:08:18 +0530

    Project Updated!!!

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Println("Server starting on port 3000!!!!")
-	err := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":4000", nil)
 	if err != nil {
 		panic(err)
 	}
